193f047fd5dcd413ffca6352a4155ce088f518be
crltsnch <crltsnch@myuax.com>
2021-12-14 11:27:57 +0100

    funcion len

--- Ejercicio banana.py
+++ Ejercicio banana.py
@@ -1,18 +1,14 @@
-def miniongame(i):
-  palabra = "banana"
-  piramide = " "
+def miniongame(string):
+  palabra = "Banana"
   jugador1stuart = 0
   jugador2kevin = 0
-  palabra = []
+  s=len(string)
 
-  for item in palabra:
-    piramide += item
-  
-  for i in range(palabra):
-    if palabra in "BCDFGHJKLMNÑPQRSTVWXYZ":
-      jugador1stuart += palabra - i
+  for i in (len(s)):
+    if s[i] in "BCDFGHJKLMNÑPQRSTVWXYZ":
+      jugador1stuart += (len(s)) - i
     elif palabra in "AEIOU":
-      jugador2kevin += palabra - i
+      jugador2kevin += (len(s)) - i
 
   if jugador1stuart > jugador2kevin:
     print("¡Stuart ha ganado!")
@@ -20,4 +16,3 @@ def miniongame(i):
     print("¡Kevin ha ganado!")
   elif jugador1stuart == jugador2kevin:
     print("Empate")
-
