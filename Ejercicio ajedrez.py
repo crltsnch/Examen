c37a8cfb830cb1488c1681883c8584e498b6a988
crltsnch <crltsnch@myuax.com>
2021-12-14 12:03:16 +0100

    codigo ruben modificado

--- Ejercicio ajedrez.py
+++ Ejercicio ajedrez.py
@@ -11,18 +11,19 @@ def torresverticales(t1, t2):
     print ("player-2" if xor != 0 else "player-1")
 
 if __name__ == '__main__':
+    os.environ['OUTPUT_PATH'] = "ayuda.txt"
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     t = int(input().strip())
     for t_itr in range(t):
         n = int(input().strip())
-        r1 = []
-        for _ in range(n):
-            r1_item = int(input().strip())
-            r1.append(r1_item)
-        r2 = []
-        for _ in range(n):
-            r2_item = int(input().strip())
-            r2.append(r2_item)
-        result = torresverticales(r1, r2)
-        fptr.write(result + '\n')
+    r1 = []
+    for _ in range(n):
+        r1_item = int(input().strip())
+        r1.append(r1_item)
+    r2 = []
+    for _ in range(n):
+        r2_item = int(input().strip())
+        r2.append(r2_item)
+    result = torresverticales(r1, r2)
+    fptr.write(result + '\n')
     fptr.close()
