9538c4b12ce9fa240a629b3bd47332c2fa040941
crltsnch <crltsnch@myuax.com>
2021-12-14 11:06:26 +0100

    Añadir los dos jugadores y crear las palabras de cada uno

--- Ejercicio banana.py
+++ Ejercicio banana.py
@@ -1,6 +1,15 @@
-palabra = "banana"
-piramide = ""
+def miniongame(i):
+  palabra = "banana"
+  piramide = " "
+  jugador1stuart = 0
+  jugador2kevin = 0
+  palabra = []
 
-for item in palabra:
-  piramide += item
-  print(piramide)
+  for item in palabra:
+    piramide += item
+  
+  for i in range(palabra):
+    if palabra in "BCDFGHJKLMNÑPQRSTVWXYZ":
+      jugador1stuart += palabra - i
+    elif palabra in "AEIOU":
+      jugador2kevin += palabra - i
